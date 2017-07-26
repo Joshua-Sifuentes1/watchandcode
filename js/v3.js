91aff4bb265dddaae4f9cb52643a83cacd9327fc
Joshua-Sifuentes1 <joshua.sifuentes1@gmail.com>
2017-07-26 15:20:54 -0500

    created addTodo method in the todoList object.

--- js/v3.js
+++ js/v3.js
@@ -1,6 +1,10 @@
 var todoList = {
-  todos: ['item 1', 'item 2', 'item 3'],
-  displayTodos: function() {
+  todos: ['item 1', 'item 2', 'item 3'], // 'property': 'value', 
+  displayTodos: function() { // method: a function stored on an object property is a method. 'property': 'method'
   	console.log('My Todos:', this.todos);
+  },
+  addTodo: function(todo) {
+  	this.todos.push(todo);
+  	this.displayTodos();
   }
 };
