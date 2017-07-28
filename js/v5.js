5a664375372c92d332b86c3c79270feb9c4931f6
Joshua-Sifuentes1 <joshua.sifuentes1@gmail.com>
2017-07-27 21:44:25 -0500

    Added an if/else condition to the displayTodo method. If the todos array is empty, the displayTodo method will display tet indicating the list is empty. If the todos array has content it will display the items as it did in version 4.

--- js/v5.js
+++ js/v5.js
@@ -1,29 +1,37 @@
 var todoList = {
-  todos: [], // 'property': 'value', 
-  displayTodos: function() { // method: a function stored on an object property is a method. 'property': 'method'
-  	console.log('My Todos:');
-    for(var i = 0; i < this.todos.length; i++) {
-      console.log(this.todos[i].todoText);
-    }
-  },
-  addTodo: function(todoText) {
-  	this.todos.push({
-      todoText: todoText,
-      completed: false
-    });
-  	this.displayTodos();
-  },
-  changeTodo: function(position, todoText) {
-  	this.todos[position].todoText = todoText;
-  	this.displayTodos();
-  },
-  deleteTodo: function(position) {
-  	this.todos.splice(position, 1);
-  	this.displayTodos();
-  },
-  toggleCompleted: function(position) {
-    var todo = this.todos[position];
-    todo.completed = !todo.completed;
-    this.displayTodos();
-  }
+	todos: [], // 'property': 'value', 
+	displayTodos: function() { // method: a function stored on an object property is a method. 'property': 'method'
+		if (this.todos.length === 0) {
+			console.log('Your todo list is empty!');	
+		} else {
+			console.log('My Todos:');
+			for(var i = 0; i < this.todos.length; i++) {
+				console.log(this.todos[i].todoText);
+			}
+		}
+		// if there is no todos
+			// console.log('Your todo list is empty ')
+		// else 
+			// print the todo list
+	},
+	addTodo: function(todoText) {
+		this.todos.push({
+			todoText: todoText,
+			completed: false
+		});
+		this.displayTodos();
+	},
+	changeTodo: function(position, todoText) {
+		this.todos[position].todoText = todoText;
+		this.displayTodos();
+	},
+	deleteTodo: function(position) {
+		this.todos.splice(position, 1);
+		this.displayTodos();
+	},
+	toggleCompleted: function(position) {
+		var todo = this.todos[position];
+		todo.completed = !todo.completed;
+		this.displayTodos();
+	}
 };
