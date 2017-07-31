48e819e3aaf3fbb2607e8a25ad3e6ff13f9044bc
Joshua-Sifuentes1 <joshua.sifuentes1@gmail.com>
2017-07-30 19:23:46 -0500

    created toggleAllButton variable that grabs the id elemnet from the dom. Created added an event listener that listens for a click which runs the toggleAll method.

--- js/v6.js
+++ js/v6.js
@@ -57,9 +57,14 @@ var todoList = {
 };
 
 var displayTodosButton = document.getElementById('displayTodosButton');
+var toggleAllButton = document.getElementById('toggleAllButton');
 
 displayTodosButton.addEventListener('click', function() {
 	todoList.displayTodos();
 });
+
+toggleAllButton.addEventListener('click', function() {
+	todoList.toggleAll();
+})
 
 
