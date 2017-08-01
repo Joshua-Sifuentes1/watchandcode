e1ea1b37ecedf9181d786039bdee45cebe17b648
Joshua-Sifuentes1 <joshua.sifuentes1@gmail.com>
2017-08-01 16:18:29 -0500

    Created toggleCompleted method and added it to the handlers object.

--- js/v8.js
+++ js/v8.js
@@ -60,9 +60,6 @@ var handlers = {
 	displayTodos: function() {
 		todoList.displayTodos();
 	},
-	toggleAll: function() {
-		todoList.toggleAll();
-	},
 	addTodo: function() {
 		var addTodoTextInput = document.getElementById('addTodoTextInput');
 		if (addTodoTextInput.value == '') {
@@ -87,6 +84,14 @@ var handlers = {
 		var deleteTodoPositionInput = document.getElementById('deleteTodoPositionInput');
 		todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber);
 		deleteTodoPositionInput.value = '';
+	},
+	toggleCompleted: function() {
+		var toggleCompletedPositionInput = document.getElementById('toggleCompletedPositionInput');
+		todoList.toggleCompleted(toggleCompletedPositionInput.valueAsNumber);
+		toggleCompletedPositionInput.value = '';
+	},
+	toggleAll: function() {
+		todoList.toggleAll();
 	}
 };
 
